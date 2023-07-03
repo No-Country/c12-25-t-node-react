b77122cf0bb71fba65ccda622a7795e5e2f746c9
Leandro Romero <romerodisind@gmail.com>
2023-07-03 16:38:28 -0300

    refactor/#2heroImage-code restructuring

--- client/src/components/heroImage/HeroImage.tsx
+++ client/src/components/heroImage/HeroImage.tsx
@@ -1,12 +1,12 @@
 import heroImage from '../../assets/heroImage/heroImage.png'
-import { Grid } from '@material-ui/core';
+import { Grid } from '@material-ui/core'
 const HeroImage = ()=> {
   return (
       <Grid container>
           <Grid item xs={12}>
               <img
                   src={heroImage}
-                  alt="Imagen"
+                  alt="Imagen de sala de estar"
                   style={{ width: '100%', height: '50vh', objectFit: 'cover' }}
               />
           </Grid>
