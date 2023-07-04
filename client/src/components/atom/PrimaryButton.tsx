9f8879d918ddba9b6e082650655997b5b93c61c6
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-07-04 14:38:32 -0300

    feat/-terms-and-conditions:  ajusto el PrimaryButton

--- client/src/components/atom/PrimaryButton.tsx
+++ client/src/components/atom/PrimaryButton.tsx
@@ -1,23 +1,33 @@
-import {
-  Button,
-  ButtonProps
-} from '@mui/material'
+import { Button, ButtonProps } from '@mui/material'
 
 type variantBtn = 'contained' | 'outlined' | 'text'
+type sizeBtn = 'small' | 'medium' | 'large'
+type colorBtn = 'inherit' | 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning'
 
 interface PrimaryButtonProps extends ButtonProps {
-  text: string,
+  text: string
   variant?: variantBtn
+  size?: sizeBtn
+  colorBtn?: colorBtn
 }
 
 const PrimaryButton: React.FC<PrimaryButtonProps> = ({
   text,
   variant,
+  size,
+  colorBtn,
   ...props
 }) => {
   return (
     <Button
       variant={ variant ? variant : 'contained' }
+      size={ size ? size : 'medium' }
+      color={ colorBtn ? colorBtn : 'primary' }
+      sx={ {
+        borderRadius: 2,
+        paddingY: 1,
+        paddingX: 1.5,
+      } }
       { ...props }
     >
       { text }
