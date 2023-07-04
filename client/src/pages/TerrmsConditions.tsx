3f80b3b0b7338ed872374f1850db4cfa6ebafe53
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-07-04 19:30:51 -0300

    #feat/#7-terms-and-conditions - add the terms and condition text

--- client/src/pages/TerrmsConditions.tsx
+++ client/src/pages/TerrmsConditions.tsx
@@ -1,12 +1,15 @@
+import {
+  Box,
+  Divider,
+  Paper,
+  Typography
+} from '@mui/material'
 import HeroImage from '../components/atom/heroImage/HeroImage'
 import termsAndConditionBanner from '../assets/terms-condition-banner.png'
+import Subtitle from '../components/atom/Subtitle'
+import { CONDITIONS, INTRODUCTION } from '../utils/terms-conditions-text'
 
 type TermsConditionsProps = {
-  /* aca van las props, ej:
-  title: string
-  Y luego se desestructuran en:
-  const TermsConditions: React.FC<TermsConditionsProps> = ({title}) => {
-  */
 }
 
 const TermsConditions: React.FC<TermsConditionsProps> = () => {
@@ -16,6 +19,38 @@ const TermsConditions: React.FC<TermsConditionsProps> = () => {
         imgSrc={ termsAndConditionBanner }
         imgHeight="23vh"
       />
+      <Box
+        sx={ {
+          width: '80%',
+          maxWidth: '800px',
+          margin: '2rem auto',
+          position: 'relative',
+          top: '-100px'
+        } }
+      >
+        <Paper
+          elevation={ 2 }
+        >
+          <Subtitle
+            title="Términos y condiciones del sitio"
+            variant="h2"
+            textTransform="none"
+            textColor="#1B17E7"
+            textAlign="center"
+          />
+          <Divider />
+          <Typography sx={ { padding: '1.5rem 1.5rem' } }>{ INTRODUCTION }</Typography>
+          { CONDITIONS && CONDITIONS.map(condition => (
+            <Box key={ condition.title } sx={{padding: '0.25rem 1.5rem 1rem'}}>
+              <Subtitle title={ condition.title } variant="h3" />
+              {
+                condition.text.map((text, index) => <Typography key={`a-${index}`} sx={{paddingBottom: '0.5rem'}}>{ text }</Typography>)
+              }
+            </Box>
+          )) }
+          <Typography></Typography>
+        </Paper>
+      </Box>
     </main>
   )
 }
