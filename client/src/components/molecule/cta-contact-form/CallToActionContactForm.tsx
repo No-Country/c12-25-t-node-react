28a496e3d6ad4e358f131deda5da89f3af7af6c3
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-07-05 20:37:25 -0300

    #feat/#6-cta-contact-form -pasamos a componentes de MUI

--- client/src/components/molecule/cta-contact-form/CallToActionContactForm.tsx
+++ client/src/components/molecule/cta-contact-form/CallToActionContactForm.tsx
@@ -1,68 +1,61 @@
 import React from 'react'
+import {
+  Box,
+  Container,
+  Typography
+} from '@mui/material'
 import './CallToActionContactForm.styles.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import PrimaryButton from '../../atom/PrimaryButton.tsx'
-import {Box, styled, Typography} from '@mui/material'
+
 
 interface ImageWithTextProps {
-  imageUrl: string;
-  textPosition: "left" | "right";
+  imageUrl: string
+  textPosition: "left" | "right"
 }
 
 const CallToActionContactForm: React.FC<ImageWithTextProps> = ({
   imageUrl,
   textPosition,
 }) => {
-  const ImageContainer = styled("img")({
-    width: "75%",
-    height: "auto",
-  });
-
-  const TextContainer = styled(Box)(({ textPosition }: { textPosition: string }) => ({
-    position: 'absolute',
-    backgroundColor: '#f9f9f9',
-    padding: '10px 30px',
-    width: '50%',
-    height: '75%',
-    display:'flex',
-    flexDirection: 'column',
-    gap: '25px',
-    borderRadius: '20px',
-    filter: 'drop-shadow(0px 5px 10px rgba(0, 0, 0, 0.25))',
-    top: '50%',
-    transform: 'translateY(-50%)',
-    ...(textPosition === 'left'
-      ? {
-        left: '0',
-      }
-      : {
-        right: '0',
-      }),
-  }));
-
   return (
-    <Box position="relative" className={`image-with-text ${textPosition}`}>
-      <ImageContainer src={imageUrl} alt="Imagen" />
-        <TextContainer textPosition={textPosition}>
-          {textPosition === "left" ? (
+    <Container maxWidth="lg" className={ `image-with-text ${ textPosition }` }>
+      <Box sx={ { position: 'relative', display: 'inline-block' } }>
+        <img src={ imageUrl } alt="Imagen" className="image-container" /> 
+        <Box
+          sx={ styleBox }
+          className={ (textPosition === 'left') ? 'box-position-left' : 'box-position-right' }
+        >
+          { textPosition === "left" ? (
             <>
-             <Typography align="left" variant="body1">¿Tenés una propiedad que <strong>comercializar</strong> ?</Typography>
-              <Link to={"/contact"}>
-                <PrimaryButton text={"Contactanos"}></PrimaryButton>
-              </Link>
+              <Typography align="left" variant="body1">¿Tenés una propiedad que <strong>comercializar</strong> ?</Typography>
+              <Link to={ "/contact" }><PrimaryButton text={ "Contactanos" } /></Link>
             </>
           ) : (
             <>
               <Typography align="left" variant="body1">¿Estás buscando <strong>invertir en un desarrollo inmobiliario</strong>?</Typography>
-              <Link to={"/contact"}>
-                <PrimaryButton text={"Contactanos"}></PrimaryButton>
-              </Link>
+              <Link to={ "/contact" }><PrimaryButton text={ "Contactanos" } /> </Link>
             </>
-          )}
-        </TextContainer>
-    </Box>
+          ) }
+        </Box>
+      </Box>
+    </Container >
 
   );
 };
 
 export default CallToActionContactForm;
+
+const styleBox = {
+  position: 'absolute',
+  top: '30%',
+  backgroundColor: '#f9f9f9',
+  zIndex: 2,
+  padding: '20px 30px',
+  width: '46%',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '25px',
+  borderRadius: '20px',
+  filter: 'drop-shadow(0px 5px 10px rgba(0, 0, 0, 0.25))',
+}
