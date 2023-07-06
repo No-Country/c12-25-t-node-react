dd2e61dfe7c480b17fc557bd6ac8cbdb6e74cb2a
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-07-06 13:42:45 -0300

    #feat/#9-banner-search-contact-favorites-pages -create the favorite page and add to the route

---
+++ client/src/pages/Favorites.tsx
@@ -0,0 +1,15 @@
+type FavoritesProps = {
+  /* aca van las props, ej:
+  title: string
+  Y luego se desestructuran en:
+  const Favorites: React.FC<FavoritesProps> = ({title}) => {
+  */
+}
+
+const Favorites: React.FC<FavoritesProps> = () => {
+  return (
+    <main>Favorites</main>
+  )
+}
+
+export default Favorites
