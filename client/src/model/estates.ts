de13b5602b4dd29ac680c9de86e327919999746f
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-07-13 10:03:42 -0300

    Paso la interface States a model

---
+++ client/src/model/estates.ts
@@ -0,0 +1,12 @@
+export interface Estates {
+  id: string
+  description: string
+  adress: string
+  area: number
+  bedrooms: number
+  bathrooms: number
+  cars: number
+  image: string
+  forRent: boolean
+  forSale: boolean
+}
