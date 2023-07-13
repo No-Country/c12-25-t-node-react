b4eb0045e96bf78a5992243db8572d9c43dc6532
Leandro Romero <romerodisind@gmail.com>
2023-07-13 12:12:37 -0300

    refactor/#11-contact-form

--- client/src/components/template/contactForm/ContactForm.tsx
+++ client/src/components/template/contactForm/ContactForm.tsx
@@ -1,47 +1,42 @@
-import { useState } from 'react';
-import { Box, TextField, Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
+import { useState } from 'react'
+import { Box, TextField, Button, Dialog, DialogTitle, DialogContent, DialogActions, InputLabel, Typography } from '@mui/material'
+import './ContactForm.styles.css'
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    lastName: '',
+    fullName: '',
     phone: '',
     email: '',
     message: '',
-  });
+  })
   const [errors, setErrors] = useState({
-    name: '',
-    lastName: '',
+    fullName: '',
     phone: '',
     email: '',
     message: '',
-  });
+  })
 
   const [openModal, setOpenModal] = useState(false);
 
   const handleChange = (e: any) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
+      fullName: e.target.value,
+    })
+  }
 
   const handleSubmit = (e: any) => {
-    e.preventDefault();
+    e.preventDefault()
 
     // Validar los campos
     const errors = {
-      name: '',
-      lastName: '',
+      fullName: '',
       phone: '',
       email: '',
       message: '',
-    };
-    if (!formData.name) {
-      errors.name = 'El nombre es obligatorio';
     }
-    if (!formData.lastName) {
-      errors.lastName = 'El apellido es obligatorio';
+    if (!formData.fullName) {
+      errors.fullName = 'El nombre es obligatorio';
     }
     if (!formData.phone) {
       errors.phone = 'El teléfono es obligatorio';
@@ -63,72 +58,72 @@ const ContactForm = () => {
     if (Object.values(errors).every((error) => error === '')) {
       setOpenModal(true);
     }
-  };
+  }
 
   const handleCloseModal = () => {
     setOpenModal(false);
-  };
+  }
 
   return (
-    <Box sx={{ maxWidth: 400 }}>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          label="Nombre"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          error={!!errors.name}
-          helperText={errors.name}
-          fullWidth
-          required
-        />
-        <TextField
-          label="Apellido"
-          name="lastName"
-          value={formData.lastName}
-          onChange={handleChange}
-          error={!!errors.lastName}
-          helperText={errors.lastName}
-          fullWidth
-          required
-        />
-        <TextField
-          label="Teléfono"
-          name="phone"
-          value={formData.phone}
-          onChange={handleChange}
-          error={!!errors.phone}
-          helperText={errors.phone}
-          fullWidth
-          required
-        />
-        <TextField
-          label="Correo Electrónico"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          error={!!errors.email}
-          helperText={errors.email}
-          fullWidth
-          required
-        />
-        <TextField
-          label="Mensaje"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          error={!!errors.message}
-          helperText={errors.message}
-          fullWidth
-          multiline
-          rows={4}
-          required
-        />
-        <Button type="submit" variant="contained" color="primary" sx={{ marginTop: 2 }}onClick={handleSubmit}>
-  Enviar
-</Button>
-      </form>
+    <Box>
+      <Box sx={{display: 'flex', flexDirection: 'column'}}>
+        
+      <form onSubmit={handleSubmit} className='formContact'>
+        <Box sx={{width: '350px',margin: '5px'}}>
+          <Typography variant="body1"sx={{color: '#0C0C39', fontSize: '16px',fontWeight: '600'}}>Nombre completo</Typography>
+            <TextField
+            label="Nombre completo"
+              name="fullName"
+              value={formData.fullName}
+              onChange={handleChange}
+              error={!!errors.fullName}
+              helperText={errors.fullName}
+              fullWidth
+              required
+            />
 
+          <Typography variant="body1"sx={{color: '#0C0C39', fontSize: '16px',fontWeight: '600'}}>Teléfono</Typography>
+            <TextField
+              label="Teléfono"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+              error={!!errors.phone}
+              helperText={errors.phone}
+              fullWidth
+              required
+              />
+          
+          <Typography variant="body1"sx={{color: '#0C0C39', fontSize: '16px',fontWeight: '600'}}>Correo Electrónico</Typography>
+            <TextField
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              error={!!errors.email}
+              helperText={errors.email}
+              fullWidth
+              required
+            />
+        </Box>
+        <Box sx={{width: '350px', height:'100%', margin: '5px'}}>
+          <Typography variant="body1"sx={{color: '#0C0C39', fontSize: '16px',fontWeight: '600'}}>Mensaje</Typography>
+            <TextField
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              error={!!errors.message}
+              helperText={errors.message}
+              fullWidth
+              multiline
+              rows={8}
+              required
+              sx={{width:'100%', height:'100%'}}
+            />
+            
+        </Box>
+      </form>
+      <Button type="submit" variant="contained" color="primary" sx={{ }}onClick={handleSubmit}>Enviar</Button>
+      </Box>
       <Dialog open={openModal} onClose={handleCloseModal}>
         <DialogTitle>¡Formulario enviado con éxito!</DialogTitle>
         <DialogContent>
--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -3,6 +3,8 @@ import contactUsBanner from '../assets/contact-us-banner.png'
 import TextBox from '../components/molecule/text-box/TextBox'
 import Subtitle from '../components/atom/Subtitle'
 import ContactForm from '../components/template/contactForm/ContactForm'
+import { CONTACT_TEXT } from '../utils/contact-form-conditions'
+import TitleText from '../components/molecule/text/Text'
 
 type ContactProps = {
   /* aca van las props, ej:
@@ -17,12 +19,22 @@ const Contact: React.FC<ContactProps> = () => {
     <BannerAndBackgroundPage imgSrc={contactUsBanner} >
       <TextBox subTitle={
         <Subtitle
-        title='Hola'
-        titleBold='hola'
-        padding='2rem 2rem 1rem'/>
+        title='Comunicate con'
+        titleBold='nosotros'
+        padding='1rem 1.1rem 1.5rem'
+        textAlign='center'/>
       }>
+        <TitleText
+        textToShow={CONTACT_TEXT}
+        paddingText="1rem 1.5rem"/>
         <ContactForm/>
+        <Subtitle
+        title='Al enviar este formulario estás aceptás los '
+        titleBold='términos y condiciones de uso y la política de privacidad.'
+        padding='1rem 1.1rem 1.5rem'
+        textColor= '#000'/>
       </TextBox>
+      
     </BannerAndBackgroundPage>
   )
 }
