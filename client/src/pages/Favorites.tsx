db9ff68bb5890bb73fb164fc0ea2330531127766
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-07-28 14:39:21 -0300

    imagen banner de favoritos

--- client/src/pages/Favorites.tsx
+++ client/src/pages/Favorites.tsx
@@ -9,7 +9,9 @@ import 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { useSpinner } from '../context/SpinnerProvider'
 import SkeletonMessage from '../components/atom/SkeletonMessage'
-import { Container } from '@mui/material'
+import { Container, Grid } from '@mui/material'
+import { Box } from '@mui/system'
+import HeroImage from '../components/atom/heroImage/HeroImage'
 
 type FavoritesProps = {}
 const db = getFirestore()
@@ -17,7 +19,6 @@ const auth = getAuth()
 
 const Favorites: React.FC<FavoritesProps> = () => {
   const { addLoading, removeLoading } = useSpinner()
-
   const [favoriteIds, setFavoriteIds] = useState<number[]>([])
 
   const checkFavoriteStatus = async (userId: string) => {
@@ -54,14 +55,24 @@ const Favorites: React.FC<FavoritesProps> = () => {
   return (
     <>
       <BackButton />
-      <BannerAndBackgroundPage imgSrc={favoriteBanner} imgHeight="350px" />
-      {filteredFav.length === 0 ? (
-        <Container maxWidth="lg" sx={{ marginTop: '3rem' }}>
+      <Box sx={ { background: '#F1F1F9' } }>
+        <Grid
+          sx={ {
+            height: '350px' ,
+            backgroundImage:'../assets/favoriteBanner.png',
+            backgroundPosition: 'center center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+          } }
+        />
+      </Box>
+      { filteredFav.length === 0 ? (
+        <Container maxWidth="lg" sx={ { marginTop: '3rem' } }>
           <SkeletonMessage messageText="No tienes favoritos todavía" />
         </Container>
       ) : (
-        <CardsWithPagination list={filteredFav} />
-      )}
+        <CardsWithPagination list={ filteredFav } />
+      ) }
     </>
   )
 }
