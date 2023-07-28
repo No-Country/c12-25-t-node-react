1c7a8745f3737594312bba2a7c131d8374909049
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-07-28 15:04:20 -0300

    home

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import CallToActionContactForm from '../components/molecule/cta-contact-form/CallToActionContactForm.tsx'
 import FeaturedAcordion from '../components/template/featuredAcordion/FeaturedAcordion.tsx'
-import ImageCtaLeft from '../assets/imageCtaRight.png'
-import ImageCtaRight from '../assets/imageCtaLeft.png'
 import Searcher from '../components/Searcher.tsx'
 import { estatesDetailList } from '../utils/EstatesDetailsList.ts'
-import { Grid } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
+import { Box, Container } from '@mui/system'
+import { Link } from 'react-router-dom'
+import PrimaryButton from '../components/atom/PrimaryButton.tsx'
 
 type HomeProps = {}
 
@@ -28,15 +28,52 @@ const Home: React.FC<HomeProps> = () => {
           backgroundRepeat: 'no-repeat',
         } }
       />
-
       <Searcher />
       <FeaturedAcordion textTitle="venta" estates={ filteredForSaleEstates } />
       <FeaturedAcordion textTitle="alquiler" estates={ filteredForRentEstates } />
-      <CallToActionContactForm imageUrl={ ImageCtaLeft } textPosition={ 'left' } />
-      <CallToActionContactForm
-        imageUrl={ ImageCtaRight }
-        textPosition={ 'right' }
-      />
+      <Container maxWidth="lg" className={ `image-with-text-left` } sx={{minHeight: '300px'}}>
+        <Box sx={ { position: 'relative', display: 'flex', justifyContent: 'center', padding: '1rem 0rem' } }>
+          <img src='../assets/imageCtaRight.png' alt="Imagen" className="image-container" />
+          <Box
+            sx={
+              {
+                position: 'absolute',
+                top: '30%',
+                backgroundColor: '#f9f9f9',
+                zIndex: 2,
+                padding: '20px 30px',
+                width: '46%',
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '25px',
+                borderRadius: '20px',
+                filter: 'drop-shadow(0px 5px 10px rgba(0, 0, 0, 0.25))',
+              }
+            }
+            className={ ('left' === 'left') ? 'box-position-left' : 'box-position-right' }
+          >
+            { 'left' === "left" ? (
+              <>
+                <Typography align="left" variant="body1">
+                  ¿Tenés una propiedad que <strong>comercializar</strong> ?
+                </Typography>
+                <Link to="/contact" aria-label="pagina de contacto">
+                  <PrimaryButton text="Contactanos" />
+                </Link>
+              </>
+            ) : (
+              <>
+                <Typography align="left" variant="body1">
+                  ¿Estás buscando <strong>invertir en un desarrollo inmobiliario</strong>?
+                </Typography>
+                <Link to="/contact" aria-label="pagina de contacto">
+                  <PrimaryButton text="Contactanos" />
+                </Link>
+              </>
+            ) }
+          </Box>
+        </Box>
+      </Container >
     </main>
   )
 }
